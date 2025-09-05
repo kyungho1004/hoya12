d3262c87b80a5caa5414f39f348e6cccdc8ed709
kyungho1004 <lee729812@gmail.com>
2025-09-05 12:54:04 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
-import streamlit as st
 
-try:
-    from bloodmap_app.app import main
-except Exception:
-    st.error("❌ 앱 모듈 임포트 중 오류가 발생했습니다. 아래 로그를 확인해주세요.")
-    import traceback
-    st.code(traceback.format_exc())
-    st.stop()
+import streamlit as st
+from hoya12.bloodmap_app.app import main
 
 if __name__ == "__main__":
-    # page_config는 app.py에서만 설정 (중복 방지)
+    st.set_page_config(page_title="피수치 해석기", layout="wide")
     main()
