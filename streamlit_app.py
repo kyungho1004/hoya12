aad2691b7904c897bc4d709d3791ed42607860db
kyungho1004 <lee729812@gmail.com>
2025-09-08 08:43:04 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-from bloodmap.app import main
+try:
+    from bloodmap.app import main
+except Exception:
+    # If the folder was renamed to bloodmap_app
+    from bloodmap_app.app import main
 
 if __name__ == "__main__":
     main()
