f0f0e59437417b0c27e7fdd770f0f4e6045c1352
kyungho1004 <lee729812@gmail.com>
2025-09-08 09:54:00 +0900

    Add files via upload

--- bloodmap_app/utils/__init__.py
+++ bloodmap_app/utils/__init__.py
@@ -1,1 +1,1 @@
-# utils
+# BloodMap package
---
+++ bloodmap_app/utils/config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+APP_TITLE = "🩸 피수치 가이드 / BloodMap v3.14+"
+PAGE_TITLE = "피수치 가이드 / BloodMap"
+MADE_BY = "제작: Hoya/GPT  ·  자문: Hoya/GPT"
+CAFE_LINK_MD = "[네이버 카페: 피수치 가이드 공식카페](https://cafe.naver.com/bloodmap)"
+FOOTER_CAFE = "© 2025 BloodMap · 문의/피드백: 피수치 가이드 공식카페"
+
+DISCLAIMER = "본 자료는 보호자의 이해를 돕기 위한 참고용 정보이며, 모든 의학적 판단은 주치의의 권한입니다."
+
+LBL_WBC="WBC(백혈구)"; LBL_Hb="Hb(혈색소)"; LBL_PLT="혈소판(PLT)"; LBL_ANC="ANC(호중구)"
+LBL_Ca="Ca(칼슘)"; LBL_P="P(인)"; LBL_Na="Na(소디움)"; LBL_K="K(포타슘)"
+LBL_Alb="Albumin(알부민)"; LBL_Glu="Glucose(혈당)"; LBL_TP="Total Protein(총단백)"
+LBL_AST="AST"; LBL_ALT="ALT"; LBL_LDH="LDH"; LBL_CRP="CRP"
+LBL_Cr="Creatinine(Cr)"; LBL_UA="Uric Acid(UA)"; LBL_TB="Total Bilirubin(TB)"
+LBL_BUN="BUN"; LBL_BNP="BNP(선택)"
+
+ORDER = [LBL_WBC, LBL_Hb, LBL_PLT, LBL_ANC, LBL_Ca, LBL_P, LBL_Na, LBL_K, LBL_Alb, LBL_Glu,
+         LBL_TP, LBL_AST, LBL_ALT, LBL_LDH, LBL_CRP, LBL_Cr, LBL_UA, LBL_TB, LBL_BUN, LBL_BNP]
+
+FEVER_GUIDE = (
+"38.0~38.5℃: 해열제 고려/경과 관찰 · 38.5℃ 이상: 병원 연락 · 39℃ 이상: 즉시 병원 방문을 권고합니다."
+)
+
+FONT_PATH_REG = "fonts/NanumGothic.ttf"
