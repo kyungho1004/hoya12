8a654aa3dedafb7e66be6cb8593ab02d41b80d0e
kyungho1004 <lee729812@gmail.com>
2025-09-08 16:05:40 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+APP_NAME = "피수치 가이드 / BloodMap"
+APP_SIGNATURE = "制作者: Hoya/GPT · 자문: Hoya/GPT"
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-try:
-    from bloodmap.app import main
-except Exception:
-    # If the folder was renamed to bloodmap_app
-    from bloodmap_app.app import main
+import streamlit as st
+from bloodmap_app.app import main
 
 if __name__ == "__main__":
     main()
