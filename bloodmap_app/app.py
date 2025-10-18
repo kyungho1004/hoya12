571bc0e3659615f8a2e62ca2d3b96627aa3d9979
kyungho1004 <lee729812@gmail.com>
2025-10-18 17:00:10 +0900

    Update app.py

--- bloodmap_app/app.py
+++ bloodmap_app/app.py
@@ -751,6 +751,11 @@ with t_home:
                 st.success("피드백 점수가 저장되었습니다. 고맙습니다!")
             else:
                 st.info("쓰기 권한이 없어 점수는 세션에만 반영됩니다. (_BASE=/mnt/data)")
+           try:
+        apm.save_feedback_atomic("home", int(st.session_state.get("home_fb_score", 0)),
+                                 st.session_state.get("home_fb_text",""), {})
+    except Exception:
+        pass
 
         # 표시: 현재 평균/표 수
         try:
